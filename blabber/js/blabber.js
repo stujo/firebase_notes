265b5b94a7569d54157acb998bd16d8aa95d079a
stujo <stujo@imagescript.com>
2014-06-26 09:33:13 -0700

    Cleaning up unused CSS and adding routing to store room name

--- blabber/js/blabber.js
+++ blabber/js/blabber.js
@@ -1,14 +1,14 @@
-var BlabberApp = angular.module("BlabberApp", []);
+var BlabberApp = angular.module("BlabberApp", ['ngRoute']);
 
 
-BlabberApp.directive('blabFocusWhenFalsy', function($timeout, $rootScope) {
+BlabberApp.directive('blabFocusWhenFalsy', function ($timeout, $rootScope) {
   return {
     restrict: 'A',
     scope: {
       checkValue: "=blabFocusWhenFalsy"
     },
-    link: function($scope, $element, attrs) {
-      $scope.$watch("checkValue", function(currentValue, previousValue) {
+    link: function ($scope, $element, attrs) {
+      $scope.$watch("checkValue", function (currentValue, previousValue) {
         if (!currentValue) {
           $element[0].focus();
           $element[0].select();
@@ -18,8 +18,20 @@ BlabberApp.directive('blabFocusWhenFalsy', function($timeout, $rootScope) {
   }
 });
 
+BlabberApp.config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.
+      when('/', {
+      }).
+      when('/in/:room_name', {
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
 
-function UserContextController($scope,$timeout) {
+
+function FullPageController($scope, $timeout, $location, $log, $rootScope, $route, $routeParams) {
 
 
   var allChatsRef = new Firebase("https://blabber.firebaseio.com/chats");
@@ -29,6 +41,29 @@ function UserContextController($scope,$timeout) {
 
 
   $scope.my = {room_name: '', screen_name: ''};
+
+  // Use location as our storage of current screen_name and room_name
+  $rootScope.$on("$locationChangeSuccess", function (event, location) {
+    $log.info("location changed to:" + location);
+    $timeout(function () {
+      refreshChatRoom($routeParams.room_name);
+    });
+  });
+
+  function refreshChatRoom(room_name) {
+    if ($scope.my.screen_name) {
+      if (room_name) {
+        $scope.my.room_name = room_name;
+      }
+      else {
+        // Log out of room_name
+        $scope.my.room_name = '';
+      }
+    }
+
+    $log.info('Current chat: as(' + $scope.my.screen_name + ") in(" + $scope.my.room_name + ")");
+    return true;
+  }
 
   $scope.$watch("my.room_name", function (newValue, oldValue) {
     if (newValue) {
@@ -49,8 +84,8 @@ function UserContextController($scope,$timeout) {
         $timeout(function () {
           var messages = [];
           // Reverse the order
-          ss.forEach(function(childSnapshot) {
-            messages.unshift( childSnapshot.val());
+          ss.forEach(function (childSnapshot) {
+            messages.unshift(childSnapshot.val());
           });
           $scope.chat.messages = messages;
         });
@@ -61,29 +96,30 @@ function UserContextController($scope,$timeout) {
     }
   });
 
-  $scope.change_screen_name = function()
-  {
+  $scope.change_screen_name = function () {
     $scope.my.screen_name = $scope.my.pending_screen_name;
   };
 
-  $scope.exit_screen_name = function ()
-  {
+  $scope.exit_screen_name = function () {
     $scope.my.screen_name = null;
+    $location.path('/');
   };
 
-  $scope.change_room_name = function()
-  {
+  $scope.change_room_name = function () {
     $scope.my.room_name = $scope.my.pending_room_name;
+    $location.path('/in/' + $scope.my.room_name);
   };
 
-  $scope.exit_room_name = function ()
-  {
+  $scope.exit_room_name = function () {
     $scope.my.room_name = null;
+    $location.path('/');
   };
-
 
 
   $scope.blab = function () {
+
+    console.log($routeParams);
+
 
     var message = $scope.my.pending_message;
     var screen_name = $scope.my.screen_name;
